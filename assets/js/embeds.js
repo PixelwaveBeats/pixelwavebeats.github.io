9b5034e1869d118e77eace4c41eedbbf10acbe20
Pixelwave Beats <pixelwave.beats@gmail.com>
2024-12-17 12:50:53 -0800

    Updates

--- assets/js/embeds.js
+++ assets/js/embeds.js
@@ -11,3 +11,13 @@ var options = {
 var player = new Twitch.Player("twitch-player", options);
 
 player.setVolume(0.5);
+
+fetch('../json/embeds.json')
+  .then(response => response.json())
+  .then(data => {
+    const videos = data.videos;
+    const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+    
+    console.log(randomVideo);
+  })
+  .catch(error => console.error('Error fetching the embeds.json file:', error));
