335c350df7403fa044e7a2b20b667c43eda44765
Pixelwave Beats <pixelwave.beats@gmail.com>
2024-12-16 16:24:28 -0800

    First Commit

---
+++ assets/js/embeds.js
@@ -0,0 +1,12 @@
+var options = {
+  width: '100%',
+  height: 500,
+  channel: "pixelwavebeats",
+  parent: [
+    "pixelwavebeats.github.io",
+  ],
+};
+
+var player = new Twitch.Player("twitch-player", options);
+
+player.setVolume(0.5);
